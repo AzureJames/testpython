2a68d13241212fc53442548c54a572246c5edd35
AzureJames <azurethegreat@gmail.com>
2022-10-24 15:32:12 -0600

    works ok

--- import.py
+++ import.py
@@ -1,5 +1,5 @@
 #  py -m pip     py -m pip install opencv-contrib-python
-print("hi")
+#ONLY ACCEPTS JPG
 import time
 import cv2
 from PIL import Image
@@ -11,10 +11,16 @@ def detect_faces(f_cascade, colored_img, scaleFactor = 1.1):
     gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)       
     #let's detect multiscale (some images may be closer to camera than others) images
     faces = f_cascade.detectMultiScale(gray, scaleFactor=scaleFactor, minNeighbors=5);   
+    numberOfFaces = 0
     #go over list of faces and draw them as rectangles on original colored img
     for (x, y, w, h) in faces:
         cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    return img_copy
+        numberOfFaces += 1
+    print(numberOfFaces)
+    if (numberOfFaces > 0):
+        return img_copy
+    else:
+        return 0
 
 
 
@@ -35,6 +41,11 @@ print(dt1)
 print(type(haar_detected_img))
 # img = Image.open(haar_detected_img)
 # img.show()
-cv2.imshow('Profile Image', haar_detected_img) 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+if(type(haar_detected_img) == int):
+    print("No faces detected")
+else:
+    print("face detected")
+    cv2.imshow('Profile Image', haar_detected_img) 
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
